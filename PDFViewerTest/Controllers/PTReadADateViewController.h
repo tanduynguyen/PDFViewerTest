82cea594982e60915234001be637bf7533b91b86
Tan Duy <tanduy.nguyen@2359media.us>
2013-02-08 11:28:42 +0700

    updated

---
+++ PDFViewerTest/Controllers/PTReadADateViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTReadADateViewController.h
+//  PDFViewerTest
+//
+//  Created by viet on 2/8/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTReadADateViewController : UIViewController
+
+@end
---
+++ PDFViewerTest/Controllers/PTReadADateViewController.m
@@ -0,0 +1,38 @@
+//
+//  PTReadADateViewController.m
+//  PDFViewerTest
+//
+//  Created by viet on 2/8/13.
+//  Copyright (c) 2013 2359media. All rights reserved.
+//
+
+#import "PTReadADateViewController.h"
+
+@interface PTReadADateViewController ()
+
+@end
+
+@implementation PTReadADateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
